fa23afbf68a3088d35138da915e66f262e03cc6e
SeongCheol Jung <kir5121@naver.com>
2025-03-13 19:29:52 +0900

    [chore] github action으로 인한 test 로그 강제출력(롤백 예정)

--- backend/src/main/java/com/project2/Project2Application.java
+++ backend/src/main/java/com/project2/Project2Application.java
@@ -1,7 +1,10 @@
 package com.project2;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -9,7 +12,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class Project2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Project2Application.class, args);
+		SpringApplication app = new SpringApplication(Project2Application.class);
+		Environment env = app.run(args).getEnvironment();
+		System.out.println("🔥 Active profiles: " + Arrays.toString(env.getActiveProfiles()));
 	}
 
 }
