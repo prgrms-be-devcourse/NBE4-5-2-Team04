ea48694da34758fedf6da6db5fa888178cd1053c
SeongCheol Jung <kir5121@naver.com>
2025-03-13 19:41:59 +0900

    [chore] gitAction 성공으로 인한 롤백

--- backend/src/main/java/com/project2/Project2Application.java
+++ backend/src/main/java/com/project2/Project2Application.java
@@ -1,10 +1,7 @@
 package com.project2;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -12,9 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class Project2Application {
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(Project2Application.class);
-		Environment env = app.run(args).getEnvironment();
-		System.out.println("🔥 Active profiles: " + Arrays.toString(env.getActiveProfiles()));
+		SpringApplication.run(Project2Application.class, args);
 	}
-
 }
