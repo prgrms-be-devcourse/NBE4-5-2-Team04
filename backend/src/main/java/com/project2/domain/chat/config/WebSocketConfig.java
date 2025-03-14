0e41029761f3c9e5443319e9d45f99ad94a67b52
theGreatKang98 <77500386+theGreatKang98@users.noreply.github.com>
2025-03-14 15:42:15 +0900

    [refactor] front url 환경변수로 변경

--- backend/src/main/java/com/project2/domain/chat/config/WebSocketConfig.java
+++ backend/src/main/java/com/project2/domain/chat/config/WebSocketConfig.java
@@ -1,21 +1,22 @@
 package com.project2.domain.chat.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-import com.project2.global.app.AppConfig;
-
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+	@Value("${custom.url.front-url}")
+	private String siteFrontUrl;
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/ws")
-			.setAllowedOrigins(AppConfig.getSiteFrontUrl())
+			.setAllowedOrigins(siteFrontUrl)
 			.withSockJS();
 	}
 
